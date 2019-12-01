f8eed1ebe27bdef1b8e96130b907cfe74f9ca08b
Paul Mayle <pmayle@mednet.ucla.edu>
2019-11-30 18:04:16 -0800

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include <Arduino.h>
 #include <TimerOne.h>
 
-// const int BUTTON_DOWN = HIGH;
-// const int BUTTON_UP = LOW;
-// const int LED_ON = 0;
-// const int LED_OFF = 1;
-// const int RELAY_ON = 0;
-// const int RELAY_OFF = 1;
-// const int buttonPin = 5;     // the number of the pushbutton pin
-// const int ledPin =  13;      // the number of the LED pin
-
-// // prod board
 const int ledBlue = 8;
 const int ledGreen = 7;
 const int ledRed = 9;
@@ -18,31 +8,27 @@ const int ledRed = 9;
 const int relay = 11;
 
 const int floatSwitch = 2;
+const int floatSwitchLow = 3;
 
 const int RELAY_OFF = LOW;
 const int RELAY_ON = HIGH;
-// const int FAN_MED = 2;
-// const int FAN_HI = 3;
 
-// const int LED_NONE = FAN_OFF;
-// const int LED_RED = FAN_LOW ;
-// const int LED_GREEN = FAN_MED;
-// const int LED_BLUE = FAN_HI;
+volatile unsigned long timeCount = 0;
+volatile unsigned long relayDelay = 101;
 
-// int countWhitePercent = 0;
+enum color
+{
+  BLACK,
+  RED,
+  GREEN,
+  BLUE,
+  YELLOW,
+  PURPLE,
+  CYAN,
+  WHITE
+};
 
-// int buttonState = 0;         // variable for reading the pushbutton status
-// bool previousStateDown = false;
-unsigned long timeCount = 0;
-// int fanState = FAN_OFF;
-// int backLightState = LED_OFF;
-// int red = LED_OFF;
-// int green = LED_OFF;
-// int blue = LED_OFF;
-// bool needToSetFan = false;
-// bool toggledLight = false;
-
-// int whiteLEDstate = LED_NONE;
+boolean lowDetector = true;
 
 // Prototypes
 void timerIsr();
@@ -53,6 +39,8 @@ void setFan();
 void displayLedColor(int color);
 void sequenceLED();
 void cycleFan();
+void flashLed(unsigned int color);
+void ledColor(unsigned int color);
 
 void setup()
 {
@@ -61,11 +49,13 @@ void setup()
   pinMode(ledGreen, OUTPUT);
   pinMode(ledBlue, OUTPUT);
   pinMode(relay, OUTPUT);
+  pinMode(floatSwitch, INPUT_PULLUP);
+  pinMode(floatSwitchLow, INPUT_PULLUP);
 
   digitalWrite(relay, RELAY_OFF);
 
   // pinMode(buttonPin, INPUT);
-  Timer1.initialize(1000);          // set a timer of length 1,000,000 microseconds (or 1S)
+  Timer1.initialize(100000);        // set a timer of length 100,000 microseconds (or 100mS)
   Timer1.attachInterrupt(timerIsr); // attach the service routine here
 
   //sequenceLED();
@@ -75,115 +65,107 @@ void setup()
 void loop()
 {
   // wait for a second
-
-  if (timeCount > 2000)
+  if (digitalRead(floatSwitch))
   {
-    Timer1.stop();
-    Timer1.restart(); //set our stopwatch to 0
-    timeCount = 0;
-    Timer1.start(); //and start it up
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    digitalWrite(ledBlue, !digitalRead(ledBlue));
-    if (digitalRead(floatSwitch))
+    // water level is high
+    ledColor(BLUE);
+    digitalWrite(relay, HIGH);
+    relayDelay = 0;
+    if (digitalRead(floatSwitchLow))
     {
-      digitalWrite(ledGreen, !digitalRead(ledGreen));
-      digitalWrite(ledRed, !digitalRead(ledRed));
-      digitalWrite(relay, !digitalRead(relay));
+      // should be low to indicate water
+      //lowDetector = false;
+    }
+  }
+  else
+  {
+    if (relayDelay > 100)
+    {
+      // we can turn off the pump
+      relayDelay = 101; // don't let it wrap ..
+
+      digitalWrite(relay, LOW);
+
+      if (lowDetector && digitalRead(floatSwitchLow))
+      {
+        flashLed(GREEN);
+      }
+      else
+      {
+        flashLed(RED);
+      }
+    }
+    else
+    {
+      // pump should be running
+      ledColor(GREEN);
+      if (lowDetector && digitalRead(floatSwitchLow))
+      {
+        // don't need to wait for the timer, the level has dropped
+        relayDelay = 101; // don't let it wrap ..
+      }
+    }
+  }
+}
+bool ledState;
+void flashLed(unsigned int color)
+{
+  if (timeCount > 6)
+  {
+    timeCount = 0;
+    ;
+    if ( (ledState = !ledState))
+    {
+      ledColor(RED);
+    }
+    else
+    {
+      ledColor(BLACK);
     }
   }
 }
 
-void loop1()
+void ledColor(unsigned int color = BLACK)
 {
 
-  // if (digitalRead(buttonPin) == BUTTON_UP) {
-  //   digitalWrite( ledPin, LED_OFF );
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledBlue, LOW);
 
-  //   showWhite();
-
-  //   if (previousStateDown) {
-  //     delay(10); // debounce
-  //     // button was down, so we can act
-  //     if (toggledLight) {
-  //       toggledLight = false;
-  //     } else {
-  //       cycleFan();
-  //       needToSetFan = true;
-  //     }
-  //     previousStateDown = false;
-  //   }
-  //   if (needToSetFan) {
-  //     if (timeCount > 1000) {
-  //       needToSetFan = false;
-  //       setFan();
-  //       Timer1.stop();
-  //       timeCount = 0;
-  //     }
-  //   }
-  // } else {
-  //   digitalWrite( ledPin, LED_ON );
-
-  //   // button must be down
-  //   if (!previousStateDown) {
-  //     delay(10); // debounce
-  //     //Just been pressed
-  //     Timer1.restart();   //set our stopwatch to 0
-  //     timeCount = 0;
-  //     Timer1.start();     //and start it up
-  //     previousStateDown = true;
-  //   } else {
-  //     if (timeCount > 500) {
-  //       //500mS so toggle the light relay
-  //       digitalWrite( relayLight, RELAY_ON );
-  //       delay(300);
-  //       digitalWrite( relayLight, RELAY_OFF );
-  //       Timer1.stop();     //and stop it up
-  //       timeCount = 0;
-  //       toggledLight = true;
-  //     }
-  //   }
-  // }
+  switch (color)
+  {
+  case BLACK:
+    break;
+  case RED:
+    digitalWrite(ledRed, HIGH);
+    break;
+  case GREEN:
+    digitalWrite(ledGreen, HIGH);
+    break;
+  case BLUE:
+    digitalWrite(ledBlue, HIGH);
+    break;
+  case YELLOW:
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledRed, HIGH);
+    break;
+  case CYAN:
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledBlue, HIGH);
+    break;
+  case PURPLE:
+    digitalWrite(ledBlue, HIGH);
+    digitalWrite(ledRed, HIGH);
+    break;
+  case WHITE:
+    digitalWrite(ledGreen, HIGH);
+    digitalWrite(ledRed, HIGH);
+    digitalWrite(ledBlue, HIGH);
+    break;
+    default:
+    break;
+  }
 }
-
-// int backLightControl(int currentState) {
-//   int led = LED_NONE;
-//   switch (currentState) {
-//     case 0:
-//       led =LED_RED;
-//       break;
-//     case 1:
-//       led = LED_GREEN;
-//       break;
-//     case 2:
-//       led = LED_BLUE;
-//       break;
-//     case 3:
-//       led = LED_RED;
-//       break;
-//     case 4:
-//       led = LED_GREEN;
-//       break;
-//     case 5:
-//       led = LED_BLUE;
-//       break;
-//     case 6:
-//       led = LED_GREEN;
-//       break;
-//     case 7:
-//       led = LED_BLUE;
-//       break;
-//     case 8:
-//      led = LED_GREEN;
-//       break;
-//     case 9:
-//     //  led = LED_GREEN;
-//       break;
-//     default:
-//       led = LED_NONE;
-//   }
-//  // led = LED_BLUE;
-//   return led;
-// }
 
 /// --------------------------
 /// Custom ISR Timer Routine
@@ -191,4 +173,5 @@ void loop1()
 void timerIsr()
 {
   timeCount++;
+  relayDelay++;
 }
